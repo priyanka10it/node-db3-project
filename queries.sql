e69e233ef23b6e67a23110a163ce6c12e6df7bb5
priyanka10it <priyanka10.it@gmail.com>
2020-11-17 01:19:14 -0700

    MVP Done

--- queries.sql
+++ queries.sql
@@ -2,8 +2,24 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+    select P.Id,P.ProductName,C.Id,C.CategoryName from Product as P
+    join Category as C on P.CategoryId=C.Id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+    select o.Id, s.CompanyName from [Order] as o
+    join Shipper as s on o.ShipVia =s.Id 
+    where o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+    select ProductName, Quantity from Product
+    join OrderDetail on Product.Id = OrderDetail.ProductId 
+    where OrderDetail.OrderId = '10251';
+
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+    select O.Id, C.CompanyName, E.LastName from [Order] as O
+    join Customer as C on O.CustomerId = C.Id
+    join Employee as E on O.EmployeeId = E.Id;
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,43 @@
 // scheme-model
+const db = require('../data/db-config.js');
+
+module.exports={
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find(){
+    return db('schemes');
+}
+
+function findById(id){
+    return db('schemes').where({id}).first();
+}
+
+function findSteps(id){
+    return db('schemes as sc')
+    .join('steps as s', 's.scheme_id','sc.id')
+    .select('s.id','sc.scheme_name','s.step_number','s.instructions')
+    .where({'sc.id':id})
+    .orderBy('s.step_number')
+}
+
+function add(scheme){
+    return db('schemes').insert(scheme)
+    .then((ids)=>{
+        return findById(ids[0]);
+    })
+}
+
+function update(changes,id){
+    return db('schemes')
+    .where({id}).update(changes);
+}
+
+function remove(id){
+    return db('schemes').where({id}).del();
+}
